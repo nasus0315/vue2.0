63e0e3db31f76356fa367f8c16bf513467335129
nasus0315 <81665517+nasus0315@users.noreply.github.com>
2022-05-17 16:17:51 +0800

    Create state.js
    state

---
+++ src/state.js
@@ -0,0 +1,37 @@
+import { observe } from './observer/index.js'
+import { proxy } from './util'
+export function initState(vm) {
+    const opts = vm.$options;
+    if (opts.props) {
+        initProps(vm)
+    };
+    if (opts.methods) {
+        initMethods(vm)
+    }
+    if (opts.data) {
+        initData(vm)
+    }
+    if (opts.computed) {
+        initComputed(vm)
+    }
+    if (opts.watch) {
+        initWatch(vm)
+    }
+}
+
+function initProps() {}
+
+function initMethods() {}
+
+function initData(vm) {
+    let data = vm.$options.data;
+    vm._data = data = typeof data === 'function' ? data.call(vm) : data;
+    for (let key in data) {
+        proxy(vm, '_data', key)
+    }
+    observe(data)
+}
+
+function initComputed() {}
+
+function initWatch() {}
